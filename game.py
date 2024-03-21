09a779836bbd985e554a1c1223b4c6908962a717
M. Agustina Olia <agustinaolia2@gmail.com>
2024-03-21 17:20:46 -0300

    Ejercicio 7.3

--- game.py
+++ game.py
@@ -1,24 +1,27 @@
 import random
 words = ["python", "programacion","computadora","codigo","desarrollo","inteligencia"]
 secret_word = random.choice(words)
-max_attempts = 10
+max_fallos = 0
 guessed_letters = []
 print ("¡Bienvenido al juego de adivinanzas!")
 print ("Estoy pensando en una palabra. ¿Puedes adivinar cual es?")
 word_displayed = "_"* len(secret_word)
 print (f"Palabra: {word_displayed}")
-for i in range (max_attempts):
+while max_fallos <=9 :
     letter = input ("Ingresa una letra: ").lower()
     if letter in guessed_letters:
         print ("Ya has intentado con esa letra. Intenta con otra.")
+        max_fallos +=1
         continue
     if letter == (""):
         print ("Valor inválido. Ingrese otro. ")
+        max_fallos += 1
         continue
     elif letter in secret_word:
         print ("¡Bien hecho! La letra esta en la palabra.")
     else:
         print ("Lo siento, la letra no esta en la palabra")
+        max_fallos+= 1
     guessed_letters.append(letter)
     letters = []
     for letter in secret_word:
@@ -32,5 +35,5 @@ for i in range (max_attempts):
         print (f"¡Felicidades! Has adivinado la palabra secreta: {secret_word}")
         break
 else:
-    print (f"¡Oh no! Has agotado tus {max_attempts} intentos")
+    print (f"¡Oh no! Has alcanzado los {max_fallos} fallos")
     print (f"La palabra secreta era: {secret_word}")
